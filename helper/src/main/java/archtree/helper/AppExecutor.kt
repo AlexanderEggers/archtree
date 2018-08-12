f5baa1820bea4cd44a1c40af3db16c915eb60b81
Mordag <alexander.eggers91@gmail.com>
2018-08-12 12:15:15 +1000

    Added AppExecutor to helper-artifact to help with app threading

---
+++ helper/src/main/java/archtree/helper/AppExecutor.kt
@@ -0,0 +1,23 @@
+package archtree.helper
+
+import android.os.Handler
+import android.os.Looper
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class AppExecutor @Inject constructor() {
+
+    val workerThread: Executor = Executors.newSingleThreadExecutor()
+    val mainThread: Executor = MainThreadExecutor()
+
+    private class MainThreadExecutor : Executor {
+        private val mainThreadHandler = Handler(Looper.getMainLooper())
+
+        override fun execute(command: Runnable) {
+            mainThreadHandler.post(command)
+        }
+    }
+}
--- helper/src/main/java/archtree/helper/JsonConverter.kt
+++ helper/src/main/java/archtree/helper/JsonConverter.kt
@@ -1,7 +1,10 @@
 package archtree.helper
 
+import android.support.annotation.WorkerThread
 import com.google.gson.Gson
 import com.google.gson.JsonArray
+import com.google.gson.reflect.TypeToken
+import java.lang.reflect.Type
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -9,10 +12,17 @@ import javax.inject.Singleton
 class JsonConverter
 @Inject constructor(private val gson: Gson) {
 
+    @WorkerThread
+    fun convertJson(value: String, type: Type): JsonArray {
+        return gson.fromJson(value, type)
+    }
+    
+    @WorkerThread
     fun toJsonTree(value: List<*>): JsonArray {
         return gson.toJsonTree(value).asJsonArray
     }
 
+    @WorkerThread
     fun toJson(value: Any): String {
         return gson.toJson(value)
     }
