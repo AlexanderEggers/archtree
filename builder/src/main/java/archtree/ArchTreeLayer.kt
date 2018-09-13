4153876e73b4312524d85dcd5a9b73c677d33bbf
Mordag <alexander.eggers91@gmail.com>
2018-09-13 22:40:05 +1000

    Removed some more CallSuper annotations

--- builder/src/main/java/archtree/ArchTreeLayer.kt
+++ builder/src/main/java/archtree/ArchTreeLayer.kt
@@ -2,38 +2,31 @@ package archtree
 
 import android.databinding.ViewDataBinding
 import android.os.Bundle
-import android.support.annotation.CallSuper
 import android.view.View
 import archtree.viewmodel.BaseViewModel
 
 abstract class ArchTreeLayer<in ViewModel : BaseViewModel> {
 
-    @CallSuper
     open fun onResume(viewModel: ViewModel?, binding: ViewDataBinding?, bundle: Bundle?) {
         //do nothing by default
     }
 
-    @CallSuper
     open fun onResume(viewModel: ViewModel?, view: View?, bundle: Bundle?) {
         //do nothing by default
     }
 
-    @CallSuper
     open fun onCreate(viewModel: ViewModel?, binding: ViewDataBinding?, bundle: Bundle?) {
         //do nothing by default
     }
 
-    @CallSuper
     open fun onCreate(viewModel: ViewModel?, view: View?, bundle: Bundle?) {
         //do nothing by default
     }
 
-    @CallSuper
     open fun onStop(viewModel: ViewModel?, binding: ViewDataBinding?) {
         //do nothing by default
     }
 
-    @CallSuper
     open fun onStop(viewModel: ViewModel?, view: View?) {
         //do nothing by default
     }
