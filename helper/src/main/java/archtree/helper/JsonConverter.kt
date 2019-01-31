7d8b1e9e5ef0d3e95a38adb39e6f4b9239254c41
Alexander Eggers <alex@domesticcat.com.au>
2019-01-31 14:36:47 +1100

    Fixed potential exception in JsonConverter.convertJson

--- helper/src/main/java/archtree/helper/JsonConverter.kt
+++ helper/src/main/java/archtree/helper/JsonConverter.kt
@@ -4,7 +4,6 @@ import androidx.annotation.WorkerThread
 import android.util.Log
 import com.google.gson.Gson
 import com.google.gson.JsonArray
-import com.google.gson.reflect.TypeToken
 import java.lang.reflect.Type
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -15,14 +14,18 @@ open class JsonConverter
 
     @WorkerThread
     open fun <T> convertJson(value: String, type: Type): T? {
-        return gson.fromJson(value, type)
+        return try {
+            gson.fromJson(value, type)
+        } catch (e: Exception) {
+            null
+        }
     }
     
     @WorkerThread
     open fun toJsonTree(value: List<*>): JsonArray? {
         return try {
             gson.toJsonTree(value).asJsonArray
-        } catch (e: IllegalStateException) {
+        } catch (e: Exception) {
             Log.wtf(JsonConverter::class.java.name, "Given JsonElement is not a JsonArray.")
             null
         }
