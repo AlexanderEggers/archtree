f8356af2bf966d3f59af2374e0237fc1d60e72c7
Mordag <alexander.eggers91@gmail.com>
2019-03-14 22:08:41 +1100

    Tweaking accessiblity helper

--- helper/src/main/java/archtree/helper/AccessibilityHelper.kt
+++ helper/src/main/java/archtree/helper/AccessibilityHelper.kt
@@ -1,7 +1,9 @@
 package archtree.helper
 
 import android.content.Context
+import android.os.Build
 import android.view.accessibility.AccessibilityManager
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import javax.inject.Inject
@@ -24,5 +26,25 @@ class AccessibilityHelper
     fun isAccessiblityEnabled(): Boolean {
         val am = context.getSystemService(Context.ACCESSIBILITY_SERVICE) as? AccessibilityManager?
         return am?.isEnabled ?: false
+    }
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    fun observeForTouchExplorationChanges(): LiveData<Boolean> {
+        val am = context.getSystemService(Context.ACCESSIBILITY_SERVICE) as? AccessibilityManager?
+        val source = MutableLiveData<Boolean>()
+
+        am?.addTouchExplorationStateChangeListener { source.value = it }
+        source.value = am?.isTouchExplorationEnabled ?: false
+
+        return source
+    }
+
+    fun isTouchExplorationEnabled(): Boolean {
+        val am = context.getSystemService(Context.ACCESSIBILITY_SERVICE) as? AccessibilityManager?
+        return am?.isTouchExplorationEnabled ?: false
+    }
+
+    fun getAccessibilityManager(): AccessibilityManager? {
+        return context.getSystemService(Context.ACCESSIBILITY_SERVICE) as? AccessibilityManager?
     }
 }
